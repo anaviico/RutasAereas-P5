3d572be0f35d52bff6466b36840f4428f8897105
Carlos Salas <csalaseiroa@correo.ugr.es>
2020-12-26 13:07:10 +0100

    Release

---
+++ include/Punto.h
@@ -0,0 +1,64 @@
+#ifndef _PUNTO_
+#define _PUTNO_
+
+#include <string>
+
+class Punto{
+private:
+
+	double lat;
+	double lng;
+	string nombre;
+
+public:
+
+	//	CONSTRUCTORES
+
+	Punto (double l, double ln){
+
+		lat = l;
+		lng = ln;
+
+	}
+
+	Punto (double l, double ln, string n){
+		
+		lat = l;
+		lng = ln;
+		nombre = n;
+
+	}
+
+
+	// OBTENCIÓN DE VALORES	
+
+	double getLat(){
+
+		return lat;
+
+	}
+
+	double getLng(){
+
+		return lng;
+
+	}
+
+
+	// SOBRECARGA DE OPERADORES
+
+	bool operator == (Punto otro){
+
+		bool igual = true;
+		if (lat != otro.getLat() || lng != otro.getLng()){
+			igual = false
+		}
+
+		return igual;
+	}
+
+};
+
+
+
+#endif
